ef36bb6f66c39eed9a349d2fe6ed9cc841b57b38
Segger <saintdane@gmail.com>
2015-12-14 16:52:21 -0500

    Map update
    Updated display viewport and some other shet

--- src/player.js
+++ src/player.js
@@ -10,9 +10,8 @@
 	Preloads assets to be used by player entities.
 */
 playerPreload = function(){
-		game.load.spritesheet('gum','gfx/gum.png',32,32)
+		game.load.spritesheet('gum','gfx/gumSheet.png',64,64);
 		game.load.spritesheet('slash','gfx/slash.png',64,64);
-		game.load.image('bg','gfx/sketch.png');
 		game.load.audio('blip','sfx/blip.wav');
 		game.load.audio('burp','sfx/burp.wav');
 }
@@ -25,19 +24,21 @@ playerPreload = function(){
 createPlayer =  function(x,y,name,controlling){
 	//Initializes entity and core properties
 	var player = game.add.sprite(x,y,name,0);
-	game.physics.enable(player,Phaser.Physics.ARCADE);
 	player.anchor.set(0.5);
+	game.physics.enable(player,Phaser.Physics.ARCADE);
+	player.body.allowGravity = false;
 	player.body.collideWorldBounds = true;
 	player.body.setSize(16,20,0,12) //(w,h,ox,oy)	
 
 	//Creates a group of slash sprites to interact with enemy
 	player.slashes = game.add.group();
 	player.slashes.enableBody= true;
+	player.slashes.setAll("allowGravity",false);
 	player.slashes.physicsBodyType = Phaser.Physics.ARCADE;
 	player.slashes.createMultiple(4,'slash');
 	player.slashes.setAll('anchor.x',0.5);
 	player.slashes.setAll('anchor.y',0.5);
-	player.scale.set(2.5);
+	player.scale.set(2);
 
 	//Setup sound effects
 	player.slashSound = game.add.audio('blip');
@@ -46,8 +47,10 @@ createPlayer =  function(x,y,name,controlling){
 	player.hitSound.allowMultiple = false;
 
 	//Defines game controls (needs to be moved to dedicated module later)
+	player.controlling = controlling;
 	player.cursors = game.input.keyboard.createCursorKeys();
 	player.atk = game.input.keyboard.addKey(Phaser.Keyboard.Z);
+	player.debug = game.input.keyboard.addKey(Phaser.Keyboard.Y); //debug key
 	player.runSpeed = 450;
 	player.slashTimer = game.time.now; 
 	player.slashSpeed = 350;
@@ -110,7 +113,7 @@ createPlayer =  function(x,y,name,controlling){
 	player.controls = function(){
 		player.body.velocity.x = 0;
 		player.body.velocity.y = 0;
-		if(!controlling){return;}
+		if(!player.controlling){return;}
 		var c = player.cursors
 		if (c.up.isDown){
 			player.body.velocity.y += -player.runSpeed;
@@ -125,13 +128,21 @@ createPlayer =  function(x,y,name,controlling){
 		else if (c.left.isDown){
 			player.body.velocity.x += -player.runSpeed;
 			player.dir = 'left';
-			player.frame = 1;
+			player.frame = 3;
 		}
 		else if (c.right.isDown){
 			player.body.velocity.x += player.runSpeed;
 			player.dir = 'right';
-			player.frame = 3;
+			player.frame = 1;
 		}
+
+		if(player.debug.isDown){
+			player.body.allowGravity = true;
+			player.controlling = false;
+			player.body.gravity.y = 20000;	
+			player.body.collideWorldBounds = false;
+		}
+
 		if (player.atk.isDown){player.slash()}
 		if(!c.up.isDown && !c.down.isDown && !c.left.isDown && !c.right.isDown){player.body.velocity.set(0)}
 	}
--- src/ring.js
+++ src/ring.js
@@ -4,17 +4,29 @@
 */
 
 ring = function(){
+	
 
 	this.preload = function(){
 		playerPreload();
+		game.load.image('bg','gfx/bigMap.png');
 	};
 
 
 	this.create = function(){
 		//Define the 'gum' player character
 		this.bg = game.add.sprite(0,0,'bg');
-		this.player = createPlayer(game.width/2-(game.width/4),game.height/2,'gum',true);
-		this.enemy = createPlayer(game.width/1.5,game.height/2,'gum',false);
+		// this.player = createPlayer(game.width/2-(game.width/4),game.height/2,'gum',true);
+		this.player = createPlayer(this.bg.width/2-(this.bg.width/4),this.bg.height/2,'gum',true);
+		this.enemy = createPlayer(this.bg.width/1.5,this.bg.height/2,'gum',false);
+		game.world.setBounds(0,0,this.bg.width,this.bg.height);
+
+		game.physics.arcade.gravity.y = 400;
+
+		game.camera.follow(this.player)
+
+		//Fullscreen options
+		// game.scale.fullScreenScaleMode = Phaser.ScaleManager.EXACT_FIT;
+		// game.input.onDown.add(()=>{game.scale.startFullScreen(false);});
 	};
 
 
@@ -31,5 +43,6 @@ ring = function(){
 }
 
 var game = new Phaser.Game(window.innerWidth-18,window.innerHeight-17, Phaser.AUTO, 'game');
+// var game = new Phaser.Game(1000,562, Phaser.AUTO, 'game');
 game.state.add("ring",ring);
 game.state.start("ring");
